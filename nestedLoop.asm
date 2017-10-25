3082403e4413953aabe859f7095dbfdf0f4480db
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-10-25 20:35:01 +0200

    Create nestedLoop.asm

---
+++ nestedLoop.asm
@@ -0,0 +1,27 @@
+.model small
+.data
+.code
+main proc  
+    
+    mov cx, 5 
+    mov bx, 5
+    l1: 
+        dec bx
+        mov dl, 1
+        add dl, 48
+        mov ah, 2h
+        int 21h
+    l2:
+        mov dl, 5
+        add dl, 48
+        
+        mov ah, 2h
+        int 21h
+    
+    loop l2
+        mov cx, bx
+    
+    loop l1
+
+endp
+end main
